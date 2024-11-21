2c89a5343f19b374e5e9107728ad9b6bba0caba2
Ruchita <ruchitas@adobe.com>
2024-11-21 16:45:24 +0530

    change3

--- blocks/form/components/square/square.js
+++ blocks/form/components/square/square.js
@@ -1,3 +1,5 @@
 export default function decorate(fieldHtml, fieldJson) {
+    const noOfDiv=fieldHtml.querySelectorAll('div').length;
+    fieldHtml.style.columnCount=noOfDiv;
     return fieldHtml;
   }
